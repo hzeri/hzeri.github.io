df9a649f51531e7a225cb8707f6fb50a750423f8
hzeri <haleyzeri@gmail.com>
2025-01-02 03:03:53 +0000

    updates

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ function toggleDropdown(sectionId) {
         content.style.display = "block";
     }
 }
+
+document.querySelectorAll(".experience-header").forEach(header => {
+    header.addEventListener("click", () => {
+        const card = header.parentElement;
+        card.classList.toggle("open");
+    });
+});
+const tabs = document.querySelectorAll(".tab-button");
+const contents = document.querySelectorAll(".tab-content");
+
+tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+        tabs.forEach(t => t.classList.remove("active"));
+        contents.forEach(content => content.classList.remove("active"));
+
+        tab.classList.add("active");
+        document.getElementById(tab.dataset.tab).classList.add("active");
+    });
+});
+
